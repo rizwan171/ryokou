01e889a84c3e57e51dfa251c3d0d257bee6ecfa3
Rizwan <rizwankhalid171@gmail.com>
2023-12-20 17:32:32 +0000

    Added background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello world</h1>
+      <Image src="/bg.jpg" alt="background" width={1920} height={1080} />
     </main>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export const NavBar = () => {
   return (
-    <header className="w-full">
-      <nav className="bg-black text-white h-12 flex items-center p-8">
+    <header className="w-full absolute">
+      <nav className="text-white h-12 flex items-center p-8">
         <span className="text-2xl font-extrabold">ryokou</span>
         <Link href="#" className="ml-auto mr-6">
           home
